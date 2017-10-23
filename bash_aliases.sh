f9c1a5c95de2bfde23657725dfe69a174eb2601f
Marcin Kubala <marcin@mkubala.pl>
2017-10-23 19:48:42 +0200

    Update profile & aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,16 +1,10 @@
 alias switch_jdk7="export JAVA_HOME=`/usr/libexec/java_home -v 1.7`"
 
-alias tomcatdbg="export JPDA_ADDRESS=8089;JPDA_TRANSPORT=dt_socket"
 alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Resources/jsc"
 
-alias showRootLog="while [ ! -f ../logs/root.log ]; do echo -n; done && tail -f ../logs/root.log"
-
-alias start-tomcat-dbg="./catalina.sh jpda start && showRootLog"
-
-alias restart-tomcat="./restart.sh && showRootLog"
-alias restart-tomcat-dbg="./shutdown.sh && ./catalina.sh jpda start && showRootLog"
-
-alias clean-install="mvn clean install -o"
-alias copy-jar-to-tomcat="ls target/ | grep -e 'mes-.*\.jar' -e 'qcadoo-.*\.jar' | xargs -I {} cp target/{} ~/qcadoo/mes/mes-application/target/tomcat-archiver/mes-application/webapps/ROOT/WEB-INF/lib/"
-
 alias scp_compressed="scp -C -o CompressionLevel=9"
+
+alias gn="git number --column -s"
+alias ga="git number add"
+
+alias beep="say -v zosia \"gotowe milordzie\""
--- profile.sh
+++ profile.sh
@@ -5,5 +5,13 @@ export EDITOR="nano"
 
 export PS1='\[$(tput setaf 2)\][\u:\w]\[$(tput setaf 3)\]$(__git_ps1 "[%s]")\[$(tput setaf 6)\]\\$ \[$(tput sgr0)\]'
 
+export GREP_OPTIONS="--color=always"
+
 source ~/.bash_aliases
 source ~/.ssh_aliases
+
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+. $(brew --prefix)/etc/bash_completion
+fi
+
+export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
